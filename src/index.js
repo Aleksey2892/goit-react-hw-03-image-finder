fd3a8aa6c6dbb6302a4e1223db639c3845587fa5
Aleksey Grygorenko <aleksey2892@gmail.com>
2020-09-09 23:28:04 +0300

    rename name in package.json, rename styles.scss to base.scss

--- src/index.js
+++ src/index.js
@@ -4,6 +4,6 @@ import ReactDOM from 'react-dom';
 import App from './components/App';
 
 import 'modern-normalize/modern-normalize.css';
-import './styles.scss';
+import './base.scss';
 
 ReactDOM.render(<App />, document.querySelector('#root'));
